af989f0b31c75a99b2e05999255f02ad779deaab
CHIRIKICHH <nikolay.omelchenko.03@gmail.com>
2022-06-10 17:45:55 +0300

    Добавил ICHelp

--- ICHelp/Models/Message.cs
+++ ICHelp/Models/Message.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ICAdmin.Services
+namespace ICHelp.Models
 {
-    class RegistrationService
+    internal class Message
     {
     }
 }
--- ICHelp/ViewModels/MainPageViewModel.cs
+++ ICHelp/ViewModels/MainPageViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ICHelp.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,6 @@ namespace ICHelp.ViewModels
 {
     internal class MainPageViewModel : BaseViewModel
     {
-
+        private ObservableCollection<Message> Messages = new ObservableCollection<Message>();
     }
 }
